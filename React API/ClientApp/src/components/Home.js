d952c49c145f640a316ad5387de32ffba66eedd7
Nick Kruize <n.kruize@gmail.com>
2020-11-10 02:03:19 +0100

    cleanup

--- React API/ClientApp/src/components/Home.js
+++ React API/ClientApp/src/components/Home.js
@@ -12,8 +12,9 @@ export class Home extends Component {
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
                 <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-                <li><Link to="/AllArtists">View All Artists</Link></li>
-                <li><Link to="/AllAlbums">View All Albums</Link></li>
+                <li><Link to="/AllPublishers">View All Publishers</Link></li>
+                <li><Link to="/AllGames">View All Games</Link></li>
+                <li><Link to="/AllGenres">View All Genres</Link></li>
             </ul>
       </div>
     );
