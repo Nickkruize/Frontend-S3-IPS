ac4f1aa5baf503f71781d1ecf05a4b3ccee6c38b
Nick Kruize <n.kruize@gmail.com>
2020-11-19 16:24:39 +0100

    placeholder text test for register

--- igdb-admin/src/tests/Register.test.js
+++ igdb-admin/src/tests/Register.test.js
@@ -1,4 +1,5 @@
-import { render} from '@testing-library/react';
+import {render} from '@testing-library/react';
+import { shallow } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Register} from '../Register';
@@ -9,52 +10,56 @@ it("renders without crashing", () => {
   })
 
 it('renders Register container', () => {
-    const {getByTestId} = render(<Register></Register>)
-    expect(getByTestId('RegisterContainer')).toBeInTheDocument();
+    const wrapper = shallow(<Register/>);
+    expect(wrapper.find('[data-testid="RegisterContainer"]').length).toEqual(1);
 });
 
 it("renders usernamelabel correctly", () =>{
-    const {getByTestId} = render(<Register></Register>)
-    expect(getByTestId('UsernameLabel')).toHaveTextContent("Username")
-    expect(getByTestId('UsernameLabel')).toBeInTheDocument();
+    const wrapper = shallow(<Register/>);
+    expect(wrapper.find('[data-testid="UsernameLabel"]').at(0).text()).toEqual('Username')
+    expect(wrapper.find('[data-testid="UsernameLabel"]').length).toEqual(1);
   })
   
   it('renders usernameinput correctly', () => {
-    const {getByTestId} = render(<Register></Register>)
-    expect(getByTestId('UsernameInput')).toBeInTheDocument();
+    const wrapper = shallow(<Register/>);
+    expect(wrapper.find('[data-testid="UsernameInput"]').at(0).props().placeholder).toEqual('Username')
+    expect(wrapper.find('[data-testid="UsernameInput"]').length).toEqual(1);
   });
 
   it("renders emaillabel correctly", () =>{
-    const {getByTestId} = render(<Register></Register>)
-    expect(getByTestId('EmailLabel')).toHaveTextContent("Email")
-    expect(getByTestId('EmailLabel')).toBeInTheDocument();
+    const wrapper = shallow(<Register/>);
+    expect(wrapper.find('[data-testid="EmailLabel"]').at(0).text()).toEqual('Email')
+    expect(wrapper.find('[data-testid="EmailLabel"]').length).toEqual(1);
   })
   
   it('renders emailinput correctly', () => {
-    const {getByTestId} = render(<Register></Register>)
-    expect(getByTestId('EmailInput')).toBeInTheDocument();
+    const wrapper = shallow(<Register/>);
+    expect(wrapper.find('[data-testid="EmailInput"]').at(0).props().placeholder).toEqual('Email')
+    expect(wrapper.find('[data-testid="EmailInput"]').length).toEqual(1);
   });
 
   it('renders passwordlabel correctly', () => {
-    const {getByTestId} = render(<Register></Register>)
-    expect(getByTestId('PasswordLabel')).toHaveTextContent("Password")
-    expect(getByTestId('PasswordLabel')).toBeInTheDocument();
+    const wrapper = shallow(<Register/>);
+    expect(wrapper.find('[data-testid="PasswordLabel"]').at(0).text()).toEqual('Password')
+    expect(wrapper.find('[data-testid="PasswordLabel"]').length).toEqual(1);
 });
 
 it('renders passwordinput correctly', () => {
-    const {getByTestId} = render(<Register></Register>)
-  expect(getByTestId('PasswordInput')).toBeInTheDocument();
+    const wrapper = shallow(<Register/>);
+    expect(wrapper.find('[data-testid="PasswordInput"]').at(0).props().placeholder).toEqual('Password')
+    expect(wrapper.find('[data-testid="PasswordInput"]').length).toEqual(1);
 });
 
 it('renders passwordconfirmlabel correctly', () => {
-    const {getByTestId} = render(<Register></Register>)
-    expect(getByTestId('PasswordConfirmLabel')).toHaveTextContent("Confirm Password")
-    expect(getByTestId('PasswordConfirmLabel')).toBeInTheDocument();
+    const wrapper = shallow(<Register/>);
+    expect(wrapper.find('[data-testid="PasswordConfirmLabel"]').at(0).text()).toEqual('Confirm Password')
+    expect(wrapper.find('[data-testid="PasswordConfirmLabel"]').length).toEqual(1);
 });
 
 it('renders passwordconfirminput correctly', () => {
-    const {getByTestId} = render(<Register></Register>)
-  expect(getByTestId('PasswordConfirmInput')).toBeInTheDocument();
+    const wrapper = shallow(<Register/>);
+    expect(wrapper.find('[data-testid="PasswordConfirmInput"]').at(0).props().placeholder).toEqual('Password confirmation')
+    expect(wrapper.find('[data-testid="PasswordConfirmInput"]').length).toEqual(1);
 });
 
 it('renders RegisterButton correctly', () => {
